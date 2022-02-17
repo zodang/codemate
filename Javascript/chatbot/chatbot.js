4dea336ab49cd7bf9268ef4a31a8f48692b90b04
zodang <1141ekdms@naver.com>
2022-02-17 22:31:56 +0900

    edit text

--- Javascript/chatbot/chatbot.js
+++ Javascript/chatbot/chatbot.js
@@ -1,32 +1,29 @@
 function check_text() {
     var value = document.getElementById("console").value;
+    var text = document.getElementById("answer");
 
     if (value == "안녕") {
-        console.log("^_^");
+        text.innerHTML = "^^...";
         document.getElementById("flog").style.backgroundImage = "url('https://t1.daumcdn.net/cfile/tistory/2758CF3957BBD6D71F')";
     }
     else if (value == "잘가") {
-        console.log("ㅜㅜ")
+        text.innerHTML = "ㅜㅜ";
         document.getElementById("flog").style.backgroundImage = "url('https://t1.daumcdn.net/cfile/tistory/216EB13457BBD6DF2D')";
     }
     else if (value == "불꺼줘") {
+        text.innerHTML = "ᴗ.ᴗᶻ ᶻ ᶻ"
         document.querySelector("body").style.backgroundColor = "rgb(80, 80, 80)";
         document.getElementById("flog").style.backgroundImage = "url(./static/images/lightoff.png)";
     }
 
     else if (value == "불켜줘") {
-        if (document.querySelector("body").style.backgroundColor == "white") {
-            console.log("no...")
-            document.getElementById("flog").style.backgroundImage = "url('./static/images/lighton2.jpg')";
-        }
-        else {
-            document.querySelector("body").style.backgroundColor = "white";
-            document.getElementById("flog").style.backgroundImage = "url('./static/images/lighton.jpg')";
-        }
+        text.innerHTML = "˘ᵕ˘"
+        document.querySelector("body").style.backgroundColor = "white";
+        document.getElementById("flog").style.backgroundImage = "url('./static/images/lighton.jpg')";
     }
 
     else {
-        console.log("???")
+        text.innerHTML = "???";
         document.getElementById("flog").style.backgroundImage = "url('https://t1.daumcdn.net/cfile/tistory/2147503C57BBD6E619')";
     }
 }
