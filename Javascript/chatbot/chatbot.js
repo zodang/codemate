fd0430e89913b5a7b7b3970d25b224b8b8394492
zodang <1141ekdms@naver.com>
2022-02-18 17:56:20 +0900

    add follow function

--- Javascript/chatbot/chatbot.js
+++ Javascript/chatbot/chatbot.js
@@ -1,29 +1,64 @@
+var follow = 0;
+
 function check_text() {
     var value = document.getElementById("console").value;
-    var text = document.getElementById("answer");
+    var flog = document.getElementsByClassName('flog')[0];
+    var text = document.getElementsByClassName("answer")[0];
 
     if (value == "안녕") {
         text.innerHTML = "^^...";
-        document.getElementById("flog").style.backgroundImage = "url('https://t1.daumcdn.net/cfile/tistory/2758CF3957BBD6D71F')";
+        flog.style.backgroundImage = "url('https://t1.daumcdn.net/cfile/tistory/2758CF3957BBD6D71F')";
     }
+
     else if (value == "잘가") {
-        text.innerHTML = "ㅜㅜ";
-        document.getElementById("flog").style.backgroundImage = "url('https://t1.daumcdn.net/cfile/tistory/216EB13457BBD6DF2D')";
+        text.innerHTML = "ㅜㅜ...";
+        flog.style.backgroundImage = "url('https://t1.daumcdn.net/cfile/tistory/216EB13457BBD6DF2D')";
     }
+
     else if (value == "불꺼줘") {
-        text.innerHTML = "ᴗ.ᴗᶻ ᶻ ᶻ"
+        text.innerHTML = "zzz..."
         document.querySelector("body").style.backgroundColor = "rgb(80, 80, 80)";
-        document.getElementById("flog").style.backgroundImage = "url(./static/images/lightoff.png)";
+        text.style.backgroundColor = "white"
+        flog.style.backgroundImage = "url('https://2.gall-img.com/hygall/files/attach/images/82/358/354/232/eae7e107387042188da08d45534ab747.jpg')";
     }
 
     else if (value == "불켜줘") {
-        text.innerHTML = "˘ᵕ˘"
+        text.innerHTML = "ㅇㅅㅇ..."
         document.querySelector("body").style.backgroundColor = "white";
-        document.getElementById("flog").style.backgroundImage = "url('./static/images/lighton.jpg')";
+        flog.style.backgroundImage = "url('https://2.gall-img.com/hygall/files/attach/images/82/358/354/232/7ade4f7baf9f19519d5dfd1fb919766a.jpg')";
     }
 
     else {
-        text.innerHTML = "???";
-        document.getElementById("flog").style.backgroundImage = "url('https://t1.daumcdn.net/cfile/tistory/2147503C57BBD6E619')";
+        text.innerHTML = "???...";
+        flog.style.backgroundImage = "url('https://t1.daumcdn.net/cfile/tistory/2147503C57BBD6E619')";
+    }
+
+
+    if (follow == 0) {
+        if (value == "따라해") {
+            follow = 1;
+            text.innerHTML = "ㅇㅋ...";
+            flog.style.backgroundImage = "url('https://2.gall-img.com/hygall/files/attach/images/82/358/354/232/cc643e1cf451ab0fea80dfd6cbf6dfb3.png')";
+        }
+    }
+
+    else if (follow == 1) {
+        if (value == "그만해") {
+            follow = 0;
+            text.innerHTML = "ㅇㅋ...";
+            flog.style.backgroundImage = "url('https://2.gall-img.com/hygall/files/attach/images/82/358/354/232/7ae17a369dd6e63a61307024c2f4254c.jpg')";
+        }
+        
+        else {
+            text.innerHTML = value + "...";
+            flog.style.backgroundImage = "url('https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FrRfa2%2FbtqYQxFQluA%2FjxGkvZoORy5TqDZjVfLVN0%2Fimg.jpg')";
+        }
+    }
+
+}
+
+function press_enter() {
+    if(window.event.keyCode == 13) {
+        check_text();
     }
 }
