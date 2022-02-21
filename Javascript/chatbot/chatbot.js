32326928059b106743e8680fd13e80747342a13a
zodang <1141ekdms@naver.com>
2022-02-21 21:26:56 +0900

    typing animation(CDN)

--- Javascript/chatbot/chatbot.js
+++ Javascript/chatbot/chatbot.js
@@ -1,12 +1,43 @@
 var follow = 0;
 
+var typing = document.getElementsByClassName('help')[0];
+ 
+    var typewriter = new Typewriter(typing, {
+        loop: true,
+        });
+
+    typewriter.typeString('개구리에게 말을 걸어보자')
+        .pauseFor(1000)
+        .deleteAll()
+        .typeString('안녕')
+        .pauseFor(1000)
+        .deleteAll()
+        .typeString('불켜줘')
+        .pauseFor(1000)
+        .deleteAll()
+        .typeString('불꺼줘')
+        .pauseFor(1000)
+        .deleteAll()
+        .typeString('따라해')
+        .pauseFor(1000)
+        .deleteAll()
+        .typeString('그만해')
+        .pauseFor(1000)
+        .deleteAll()
+        .typeString('잘가')
+        .pauseFor(1000)
+        .deleteAll()
+        .start();
+
 function check_text() {
     var value = document.getElementById("console").value;
     var flog = document.getElementsByClassName('flog')[0];
     var text = document.getElementsByClassName("answer")[0];
 
+    
+
     if (value == "안녕") {
-        text.innerHTML = "^^...";
+        text.innerHTML = "^^...";        
         flog.style.backgroundImage = "url('https://t1.daumcdn.net/cfile/tistory/2758CF3957BBD6D71F')";
     }
 
@@ -33,7 +64,7 @@ function check_text() {
         flog.style.backgroundImage = "url('https://t1.daumcdn.net/cfile/tistory/2147503C57BBD6E619')";
     }
 
-
+    //따라하기
     if (follow == 0) {
         if (value == "따라해") {
             follow = 1;
@@ -54,11 +85,15 @@ function check_text() {
             flog.style.backgroundImage = "url('https://img1.daumcdn.net/thumb/R1280x0/?scode=mtistory2&fname=https%3A%2F%2Fblog.kakaocdn.net%2Fdn%2FrRfa2%2FbtqYQxFQluA%2FjxGkvZoORy5TqDZjVfLVN0%2Fimg.jpg')";
         }
     }
-
 }
 
+//엔터로 질문하기
 function press_enter() {
     if(window.event.keyCode == 13) {
         check_text();
     }
 }
+
+
+
+
--- practice/index.js
+++ practice/index.js
@@ -1,14 +1,15 @@
-function changeText1() {
-    var s = document.getElementById('one')
-    s.innerHTML = "모처럼이니"
-}
+var app = document.getElementById('app');
 
-function changeText2() {
-    var s = document.getElementById('two')
-    s.innerHTML = "각잡고 열심히 하면"
-}
+var typewriter = new Typewriter(app, {
+    loop: false,
+});
 
-function changeText3() {
-    var s = document.getElementById('three')
-    s.innerHTML = "코린이 탈출"
-}
+typewriter.typeString('Hello World!')
+    .pauseFor(2500)
+    .deleteAll()
+    .typeString('Strings can be removed')
+    .pauseFor(2500)
+    .deleteChars(7)
+    .typeString('<strong>altered!</strong>')
+    .pauseFor(2500)
+    .start();
