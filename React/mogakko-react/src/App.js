4110ac64f9ba28e6dc14f67db0fdc45363aabbbb
zodang <1141ekdms@naver.com>
2022-03-05 16:58:25 +0900

    Day 4

--- React/mogakko-react/src/App.js
+++ React/mogakko-react/src/App.js
@@ -1,12 +1,31 @@
-import Hello from './Hello';
-import Bye  from './Bye';
+import './App.css';
+import styles from './App.module.css';
+//React로부터 useState 함수 불러오기
+import React, {useState} from 'react';
 
 function App() {
+  //함수 호출
+  const [number, setNumber] = useState(0);
+  
+  function Increase() {
+    setNumber(number+1);
+  }
+
+  function Reset() {
+    setNumber(number*0);
+  }
+
+  function Decrease() {
+    setNumber(number-1);
+  }
+
   return (
-    <div>
-      <Hello />
-      <Bye />
-      </div>
+    <div className='container'>
+      <div>값: <span className={styles.result}>{number}</span></div>
+      <button onClick={Decrease}>-1</button>
+      <button onClick={Reset}>reset</button>
+      <button onClick={Increase}>+1</button>
+    </div>
   );
 }
 
