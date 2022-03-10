cd0cfb100bd5559d9a7215e737318da4d8639491
zodang <1141ekdms@naver.com>
2022-03-10 23:10:47 +0900

    Day 8

--- React/mogakko-react/src/App.js
+++ React/mogakko-react/src/App.js
@@ -1,21 +1,31 @@
 //React로부터 useState 함수 불러오기
 import React, {useState} from 'react';
 import Button from './Button';
+import './App.css';
 import {Layout, btnLayout} from './Layout';
 import Number from './Number';
 
 function App() {
   //함수 호출
   const [state, setState] = useState(false);
+  const [color, setColor] = useState("green");
   
   function checkState() {
     setState(!state);
+    
+    if (state) {
+      setColor = 'green';
+    }
+    else {
+      setColor = 'red';
+    }
   }
 
   return (
-    <div>
-      <Button onClick={checkState} text="True?"></Button>
-      {state===true &&<div>True!</div>}
+    <div id='container'>
+      <div id="box" className={state ? 'green': 'red' } />
+      <Button onClick={checkState} text="색 바꾸기!"></Button>
+      {state ? <div>True에용</div> : <div>False에용</div>}
     </div>
   );
 }
--- React/youtube-clone/src/components/shared/Header.js
+++ React/youtube-clone/src/components/shared/Header.js
@@ -1,7 +1,28 @@
+import youtube_logo from '../../data/youtube_logo.png'
 import styles from './Header.module.css';
+//아이콘
+import { FiMenu } from 'react-icons/fi';
+import { IoSearchOutline } from 'react-icons/io5';
+import { BsGrid3X3Gap } from 'react-icons/bs';
+import { HiOutlineDotsVertical } from 'react-icons/hi';
 
 function Header() {
-    return <div>헤더!</div>;
+    return (
+    <div className={styles.header}>
+        <div className={styles.tab}>
+            <FiMenu className={styles.icon} />
+            <img src={youtube_logo} alt="로고" className={styles.logo}></img>
+        </div>
+        <div className={styles['center-tab']}>
+            <input className={styles.input}/>
+            <IoSearchOutline className={styles['search-icon']} />
+        </div>
+        <div className={styles.tab}>
+            <BsGrid3X3Gap />
+            <HiOutlineDotsVertical />
+        </div>
+    </div>
+    );
 }
 
 export default Header;
