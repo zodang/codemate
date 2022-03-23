08e026754a64683bf0c3c6f5c8ecabf9aab1b7a3
zodang <1141ekdms@naver.com>
2022-03-23 14:11:11 +0900

    edit

--- React/youtube-clone/src/index.js
+++ React/youtube-clone/src/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {HashRouter} from 'react-router-dom';
-
+import {  HashRouter } from 'react-router-dom';
 ReactDOM.render(
   <React.StrictMode>
     <HashRouter>
       <App />
     </HashRouter>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
--- React/youtube-clone/src/pages/Button.js
+++
@@ -1,8 +0,0 @@
-function Button({onClick, text}) {
-    return (
-        //props객체로부터 받아온 요소(onClick, text) 사용
-        <button onClick={onClick}>{text}</button>
-    )
-}
-
-export default Button
